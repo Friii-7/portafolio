e017abed4657f0fef99ed6976b1dbb98865f2ff0
farzin jimenez <farzinjimenez92@gmail.com>
2023-07-19 13:28:43 -0500

    actualizar

---
+++ imagenes/iconos/Brandmark - make your logo in minutes_files/icon.php
@@ -0,0 +1,13 @@
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="200px" height="200px" viewBox="0 0 200 200">
+    <defs>
+      <filter id="colorFill" color-interpolation-filters="sRGB">
+          <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0.070588235294118
+                  0 0 0 0 0.070588235294118
+                  0 0 0 0 0.070588235294118
+                  0 0 0 1 0 " />
+      </filter>
+    </defs>
+    <image id="icon" xlink:href="https://app.brandmark.io/nounpreview/2998697.png" style="filter: url(#colorFill);" transform="translate(20 20) scale(0.8)" />
+</svg>
